086dce65a2fea383c22cc73822cbd2a311e602ef
thaiminhtuan135 <tuana11ht@gmail.com>
2023-03-20 14:58:34 +0700

    learn stream in java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,94 +1,119 @@
-import example.demo.model.Student;
+//package com.gpcoder.collectors;
 
 import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+class Employee {
+    private String name;
+    private Integer age;
+    private String companyName;
+    private Integer salary;
+
+    public Employee(String name, Integer age, String companyName, Integer salary) {
+        this.name = name;
+        this.age = age;
+        this.companyName = companyName;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+}
 
 public class Main {
-    private static List<Person> getPerson() {
-        return List.of(
-                new Person("tuan", 20 ,Gender.MALE),
-                new Person("hello", 21 ,Gender.MALE),
-                new Person("halland", 22 ,Gender.FEMALE),
-                new Person("james", 23 ,Gender.FEMALE),
-                new Person("messi", 24 ,Gender.MALE),
-                new Person("ronaldo", 126 ,Gender.FEMALE)
-        );
-    }
+
     public static void main(String[] args) {
-        List<Person> studentAddress = new ArrayList<>();
-        List<Person> persons = getPerson();
-//        for (Student student :students) {
-//            if (student.getAddress().equals("bac ninh")) {
-//                studentAddress.add(student);
-//            }
-//        }
-//        studentAddress.forEach(System.out::println);
-        // filter
-//        students.stream().filter(student -> student.getAddress().equals("bac ninh"))
-//                .collect(Collectors.toList()).forEach(x -> System.out.println(x));
 
-        //sort
-//        students.stream().sorted((o1, o2) -> o1.getAddress().compareTo(o2.getAddress())).forEach(x-> System.out.println(x));
+        List<Employee> list = Arrays.asList( //
+                new Employee("Emp1", 22, "A", 50), //
+                new Employee("Emp2", 23, "A", 60), //
+                new Employee("Emp3", 22, "B", 40), //
+                new Employee("Emp4", 21, "B", 70) //
+        );
+        // Find employees with the maximum age of each company
+        Comparator<Employee> ageComparator = Comparator.comparing(Employee::getAge);
 
-        // sort reversed
-//        students.stream().sorted(Comparator.comparing(Student::getAddress).reversed()).forEach(x -> System.out.println(x));
-        // allMatch
-//        boolean math = students.stream().allMatch(x -> x.getAge() > 8);
-        //anyMath
-//        boolean math = students.stream().anyMatch(x -> x.getAge() > 8);
-        //noneMath
-//        boolean math = students.stream().noneMatch(x -> x.getAddress().equals("lalala")); // -> true
-//        System.out.println(math);
 
-        //max
-//        persons.stream().max(Comparator.comparing(Person::getAge)).ifPresent(System.out::println);
+        Map<String, Optional<Employee>> map = list.stream().collect(
+                Collectors.groupingBy(Employee::getCompanyName,
+                        Collectors.reducing(BinaryOperator.maxBy(ageComparator))));
 
-//        group by
-//        Map<Gender, List<Person>> groupByGender = persons.stream().collect(Collectors.groupingBy(Person::getGender));
-//        System.out.println(groupByGender);
-//        groupByGender.forEach((gender, people) -> {
-//            System.out.println(gender);
-//            people.forEach(System.out::println);
-//            System.out.println();
-//        });
+        map.forEach((k, v) -> System.out.println(
+                "Company: " + k +
+                        ", Age: " + ((Optional<Employee>) v).get().getAge() +
+                        ", Name: " + ((Optional<Employee>) v).get().getName()));
 
-//        chain
-//        Optional<String> oldestFemaleAge = persons.stream()
-//                .filter(person -> person.getGender().equals(Gender.FEMALE))
-//                .max(Comparator.comparing(Person::getAge))
-//                .map(Person::getName);
-//        oldestFemaleAge.ifPresent(System.out::println)
-//
-//
-//        generate stream
-//        Stream<String> streamGenerated =
-//                Stream.generate(() -> "element").limit(10);
-//        streamGenerated.forEach(x-> System.out.println(x));
+        // Summary salary
 
-        // stream to map
-        List<String> list = Arrays.asList("Java", "C++", "C#", "PHP");
-//        Map<String, Integer> result = list.stream().collect(Collectors.toMap(Function.identity(), String::length));
-//        result.forEach((s, integer) -> {
-//            System.out.println(s);
-//            System.out.println(integer);
-//        });
+//        Integer bonus = 30;
+//        Integer totalSalaryExpense = list.stream()
+//                .map(emp -> emp.getSalary())
+//                .reduce(bonus, (a, b) -> a + b);
+//        System.out.println("Total salary expense: " + totalSalaryExpense);
+        FloatFunction<Float> a = (x) -> (float) x;
+        float b = a.apply(12);
+        System.out.println(b);
 
-        // IntSummaryStatistics{count=4, sum=12, min=2, average=3.000000, max=4}
-//        IntSummaryStatistics rs = list.stream().collect(Collectors.summarizingInt(String::length));
-//        System.out.println(rs);
-        Map<Integer, String> hashMap = new HashMap<>();
-        hashMap.put(1, "tuan");
-        hashMap.put(2, "tuan");
-        hashMap.put(3, "tuan");
-        hashMap.put(4, "tuan");
-        hashMap.put(5, "tuan");
-        hashMap.forEach((integer, s) -> {
-            System.out.println(s);
-            System.out.println(integer);
-        });
+//        joinString<String, String, Integer> nameer = (firstname, lastname) -> Integer.parseInt(firstname) + Integer.parseInt(lastname);
+//        int y = nameer.apply("200", "200");
+//        System.out.println(y);
+
+        lengthString<String, Integer> string = (text) -> text.length();
+
+        int leng = string.apply("tuandeotrai");
+        System.out.println(leng);
+
+
+        Function<String, String> func = text -> text.toUpperCase();
+        List<String> listLanguage = Arrays.asList("Java", "C#", "Python");
+        List<String> newList = map(func, listLanguage);
+        newList.forEach(System.out::println);
+
+
     }
 
+    @FunctionalInterface
+    public interface FloatFunction<R> {
+        R apply(float value);
+    }
+
+    @FunctionalInterface
+    public interface joinString<T, U, R> {
+        R apply(T t, U u);
+
+        default <V> java.util.function.BiFunction<T, U, V> andThen(Function<? super R, ? extends V> after) {
+            Objects.requireNonNull(after);
+            return (T t, U u) -> after.apply(apply(t, u));
+        }
+    }
+
+    @FunctionalInterface
+    public interface lengthString<T, R> {
+        R apply(T t);
+    }
+
+    public static <T, R> List<R> map(Function<T, R> mapper, List<T> list) {
+        List<R> rs = new ArrayList<R>();
+        for (T t: list) {
+            R r = mapper.apply(t);
+            rs.add(r);
+        }
+        return rs;
+    }
 }
