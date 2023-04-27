5608f8bf88e8f2d9888300b465f9329ba9c7386e
thaiminhtuan135 <tuana11ht@gmail.com>
2023-04-27 20:26:34 +0700

    remove file

--- src/main/java/example/demo/DemoApplication.java
+++ src/main/java/example/demo/DemoApplication.java
@@ -1,14 +1,8 @@
 package example.demo;
 
-import example.demo.model.Person;
-import example.demo.repository.AddressRepository;
-import example.demo.repository.PersonRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 //@RequiredArgsConstructor
